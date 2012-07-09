620397339a8ee7ba32b7379cb3ee0bd3a520204e
David Corne <davidcorne@gmail.com>
2012-07-09 12:10:34 +0100

    Iterator started

--- Iterator.py
+++ Iterator.py
@@ -1,9 +1,36 @@
 #!/usr/bin/env python
 # Written by: DGC
 
-class something(object):
-    
-    def __init__(self):
-        print("initalised")
+class ReverseIterator(object):
+    """ 
+    Iterates the object given to it in reverse so it shows the difference. 
+    """
 
+    def __init__(self, iterable_object):
+        self.list = iterable_object
+        self.index = len(iterable_object)
 
+    def __iter__(self):
+        return self
+
+    def next(self):
+        " Return the list backwards so it's noticably different."
+        if (self.index == 0):
+            # the list is over, raise a stop index exception
+            raise StopIteration
+        self.index = self.index - 1
+        return self.list[self.index]
+
+if (__name__ == "__main__"):
+    days = [
+        "Monday",
+        "Tuesday", 
+        "Wednesday", 
+        "Thursday",
+        "Friday", 
+        "Saturday", 
+        "Sunday"
+        ]
+    it = ReverseIterator(days)
+    for day in it:
+        print(day)
