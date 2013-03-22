64b12f12519e689201b9a2c6eadf45091662c4e3
David Corne <davidcorne@gmail.com>
2013-03-22 13:35:28 +0000

    Commit MonoState

---
+++ Behavioural/MonoState.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# Written by: DGC
+
+# python imports
+
+#==============================================================================
+class MonoState(object):
+    __data = 5
+    
+    def __init__(self):
+        pass
+
+    @property
+    def data(self):
+        return MonoState.__data
+
+    @data.setter
+    def data(self, value):
+        MonoState.__data = value
+
+#==============================================================================
+if (__name__ == "__main__"):
+    m_1 = MonoState()
+    print("First data: " + str(m_1.data))
+    m_1.data = 4
+    m_2 = MonoState()
+    print("Second data: " + str(m_2.data))
+    # not a singleton, so these are different instances
+    print("First instance: " + str(m_1))
+    print("Second instance: " + str(m_1))
+    
